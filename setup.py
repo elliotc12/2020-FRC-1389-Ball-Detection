d18695a46a5ad6b026802bfb4272e180b3f9679a
Elliott Capek <elliotc12@gmail.com>
2020-01-22 18:31:35 -0500

    Initial commit with scripts

---
+++ setup.py
@@ -0,0 +1,7 @@
+#this script will:
+    # take raw supervisely tar.gz
+    # untar it, convert to the data/train data/test structure
+    # convert JSON to CSV, then CSV to .record
+    # create .pbtxt file in training/sdfsd (or data not sure)
+    # create training/ folder
+    # 
